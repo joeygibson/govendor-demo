6177a53657e12d5663be65d535497134734ec0ff
Joey Gibson <joey@joeygibson.com>
2017-03-06 23:44:27 -0500

    added update program

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "godep-demo",
+		Short: "Showing how godep works",
+		Long:  "Showing how godep works",
+		Run:   CmdRoot,
+	}
+)
+
+func CmdRoot(cd *cobra.Command, args []string) {
+	log.Info("Important work")
+}
 
 func main() {
 	log.Info("Starting up")
-	log.Info("Important work")
+
+	rootCmd.Flags().StringP("key", "k", "", "Github API key")
+
+	rootCmd.Execute()
+
 	log.Info("Done")
 }
