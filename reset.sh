086cbae9a92e3e2c272f6ef717def976e79ca856
Joey Gibson <joey@joeygibson.com>
2017-04-25 12:59:11 -0400

    made reset script quiet

--- reset.sh
+++ reset.sh
@@ -5,9 +5,6 @@ rm -rf vendor
 rm -f govendor-demo
 
 for lib in github.com/Sirupsen github.com/sirupsen github.com/spf13/cobra; do
-  echo "Deleting $lib"
-  set -x
   rm -rf $GOPATH/src/$lib
   rm -rf $GOPATH/pkg/darwin_amd64/$lib
-  set +x
 done
